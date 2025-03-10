745db6cd2dc9f98a6e3e9759397d3e7c015c8dcc
Dave Kadziola <194847688+DaveKadziola@users.noreply.github.com>
2025-03-10 17:01:43 +0100

    moved handle_request_updates() to run.py due to problems with accessing app.app_context()

--- app/sockets/socket_notifications.py
+++ app/sockets/socket_notifications.py
@@ -1,4 +1,3 @@
-from app import create_app
 from ..extensions import socketio
 from ..utils.config import DB_CONFIG, CURRENT_SCHEMA
 from flask_socketio import emit
@@ -21,46 +20,3 @@ def handle_disconnect():
 
 def emit_update(event_type, data):
     socketio.emit("update", {"type": event_type, "data": data})
-
-
-@socketio.on("request_updates")
-def handle_request_updates(last_timestamp):
-    with app.app_context():
-        try:
-            conn = psycopg2.connect(
-                host=DB_CONFIG["host"],
-                port=DB_CONFIG["port"],
-                user=DB_CONFIG["user"],
-                password=DB_CONFIG["password"],
-                database=DB_CONFIG["dbname"],
-                options=f"-c search_path={CURRENT_SCHEMA}",
-            )
-            conn.set_isolation_level(
-                psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-
-            cursor = conn.cursor()
-            cursor.execute(
-                """
-                SELECT id, action, event_data, created_at
-                FROM prod.updates_log 
-                WHERE created_at > %s 
-                ORDER BY id ASC, created_at ASC
-                """, (last_timestamp, ))
-
-            for update_id, action, event_data, timestamp in cursor:
-                socketio.emit(action, {
-                    'request_updates': True,
-                    'update_id': update_id,
-                    'timestamp': timestamp.isoformat(),
-                    **event_data
-                },
-                              room=request.sid)
-
-        except psycopg2.Error as e:
-            print(f"Database error: {e}")
-            emit("sync_error", {"message": str(e)})
-        finally:
-            if 'cursor' in locals():
-                cursor.close()
-            if conn and not conn.closed:
-                conn.close()
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ eventlet.monkey_patch()
 
 from app import create_app, socketio
 from app.utils.config import DB_CONFIG, CURRENT_SCHEMA, APP_HOST_NAME, APP_PORT
+from flask import request
 
 import psycopg2
 import json
@@ -42,6 +43,49 @@ def listen_to_db():
                     socketio.emit(event_name, data)
                 else:
                     socketio.emit("update", data)
+
+
+@socketio.on("request_updates")
+def handle_request_updates(last_timestamp):
+    with app.app_context():
+        try:
+            conn = psycopg2.connect(
+                host=DB_CONFIG["host"],
+                port=DB_CONFIG["port"],
+                user=DB_CONFIG["user"],
+                password=DB_CONFIG["password"],
+                database=DB_CONFIG["dbname"],
+                options=f"-c search_path={CURRENT_SCHEMA}",
+            )
+            conn.set_isolation_level(
+                psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+            cursor = conn.cursor()
+            cursor.execute(
+                """
+                SELECT id, action, event_data, created_at
+                FROM prod.updates_log 
+                WHERE created_at > %s 
+                ORDER BY id ASC, created_at ASC
+                """, (last_timestamp, ))
+
+            for update_id, action, event_data, timestamp in cursor:
+                socketio.emit(action, {
+                    'request_updates': True,
+                    'update_id': update_id,
+                    'timestamp': timestamp.isoformat(),
+                    **event_data
+                },
+                              room=request.sid)
+
+        except psycopg2.Error as e:
+            print(f"Database error: {e}")
+            emit("sync_error", {"message": str(e)})
+        finally:
+            if 'cursor' in locals():
+                cursor.close()
+            if conn and not conn.closed:
+                conn.close()
 
 
 if __name__ == "__main__":
